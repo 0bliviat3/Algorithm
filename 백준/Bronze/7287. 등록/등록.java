f54de900c4e4b19f75f1f55e1b2b526dc7d59fb9
0bliviat3 <122261127+0bliviat3@users.noreply.github.com>
2023-09-14 23:08:57 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11188 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(103);
+		sb.append("\n");
+		sb.append("0bliviate");
+		System.out.print(sb);
+
+	}
+
+}
