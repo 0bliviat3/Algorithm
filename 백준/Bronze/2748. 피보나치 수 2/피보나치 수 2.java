7cd02d6f37107ff6af0e1bb2707fe8a0e7e46d66
0bliviat3 <122261127+0bliviat3@users.noreply.github.com>
2023-09-14 23:04:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 84 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	private long[] arr = new long[100];
+	
+	public long fiboNum(int n) {
+		if(n == 0) {
+			return 0;
+		}else if(n == 1) {
+			return 1;
+		}else {
+			if(this.arr[n] != 0) {
+				return  this.arr[n];
+			}else {
+				arr[n] = fiboNum(n-1) + fiboNum(n-2);
+				return arr[n];
+			}
+		}
+	}
+	
+	public static void main(String[] arg0) throws IOException{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		Main f = new Main();
+		int n = Integer.parseInt(in.readLine());
+		sb.append(f.fiboNum(n));
+		System.out.print(sb);
+	}
+	
+	
+}
