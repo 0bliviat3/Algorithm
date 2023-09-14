350dc047b3540c2743ca2ddb748633eb2b02bfe4
0bliviat3 <122261127+0bliviat3@users.noreply.github.com>
2023-09-14 23:08:33 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 76 ms, Memory: 11404 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		boolean[] student = new boolean[30];
+		for(int i=0;i<28;i++) {
+			int temp = Integer.parseInt(br.readLine());
+			student[temp-1] = true;
+		}
+		for(int i=0;i<30;i++) {
+			if(!student[i]) {
+				sb.append(i+1);
+				sb.append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+
+}
