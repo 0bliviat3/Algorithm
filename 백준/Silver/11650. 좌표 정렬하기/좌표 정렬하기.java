80278a7b989caf23b355f0ede77be7943e75be68
0bliviat3 <122261127+0bliviat3@users.noreply.github.com>
2023-09-14 23:17:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 560 ms, Memory: 66280 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private int[] arr = null; // 병합정렬한 x값 저장할 배열
+	private int[] arrY = null;
+
+	Main(int n) {
+		arr = new int[n];
+		arrY = new int[n];
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int n = Integer.parseInt(in.readLine());
+		int[] x = new int[n];
+		int[] y = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			x[i] = Integer.parseInt(st.nextToken());
+			y[i] = Integer.parseInt(st.nextToken());
+		}
+
+		in.close();
+
+		StringBuilder sb = new StringBuilder();
+
+		// 선택정렬 >> 시간초과
+		// 병합정렬로 두개 배열을 동시 정렬하기로함
+		// x좌표가 같다면 y좌표 비교
+
+		Main m = new Main(n);
+		m.mySort(x, y, 0, n - 1);
+		for (int i = 0; i < n; i++) {
+			sb.append(x[i] + " " + y[i] + "\n");
+		}
+
+		System.out.print(sb);
+
+	}
+
+	private void mySort(int[] x, int[] y, int left, int right) {
+		if (left == right) {
+			return;
+		}
+		int mid = (left + right) / 2;
+
+		mySort(x, y, left, mid);
+		mySort(x, y, mid + 1, right);
+
+		merge(x, y, left, mid, right);
+	}
+
+	private void merge(int[] x, int[] y, int left, int mid, int right) {
+		int l = left;
+		int r = mid + 1;
+		int idx = left;
+
+		while ((l <= mid) || (r <= right)) {
+			if (r > right || (x[l] == x[r] && y[l] < y[r]) || (l <= mid && x[l] < x[r])) {
+				arrY[idx] = y[l];
+				arr[idx++] = x[l++];
+			} else {
+				arrY[idx] = y[r];
+				arr[idx++] = x[r++];
+			}
+		}
+
+		for (int i = left; i <= right; i++) {
+			x[i] = arr[i];
+			y[i] = arrY[i];
+
+		}
+	}
+
+}
