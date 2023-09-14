2753460d5e6124a79310a7782106c66434bf7b23
0bliviat3 <122261127+0bliviat3@users.noreply.github.com>
2023-09-14 23:11:30 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 13900 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		String dog = "|\\_/|\n"
+				+ "|q p|   /}\n"
+				+ "( 0 )\"\"\"\\\n"
+				+ "|\"^\"`    |\n"
+				+ "||_/=\\\\__|";
+		sb.append(dog);
+		System.out.println(sb);
+
+	}
+
+}
