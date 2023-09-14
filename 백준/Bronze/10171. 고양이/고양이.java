4f9d853ef8240766df38fbffa939f15d1c255dc1
0bliviat3 <122261127+0bliviat3@users.noreply.github.com>
2023-09-14 23:11:24 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		String cat = "\\    /\\\n"
+				+ " )  ( ')\n"
+				+ "(  /  )\n"
+				+ " \\(__)|";
+		sb.append(cat);
+		System.out.println(sb);
+
+	}
+
+}
