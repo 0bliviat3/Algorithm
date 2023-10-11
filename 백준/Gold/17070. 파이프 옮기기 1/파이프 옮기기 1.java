1e95cd914a41d6413921b58115933f1a0da95649
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2023-10-11 09:29:35 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 148 ms, Memory: 13372 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,4 +1,3 @@
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -46,33 +45,22 @@ class BOJ17070Sol {
 		return metrix[n - 1][n - 1];
 	}
 	
-	private void dfs(Pipe pipe) {
-		int x = pipe.endPoint.x;
-		int y = pipe.endPoint.y;
+	private void dfs(Boolean way,int x, int y) {
 		if(x == n - 1 && y == n - 1) {
 			ans++;
 			return;
 		}
 		if(x == n || y == n ||metrix[x][y]) { return; }
 		
-		if(pipe.way == null || pipe.way) { dfs(new Pipe(true, new Point(x + 1, y))); }
-		if(pipe.way == null || !pipe.way){ dfs(new Pipe(false, new Point(x, y + 1))); }
-		if(x + 1 < n && y + 1 < n && !metrix[x][y + 1] && !metrix[x + 1][y]) { dfs(new Pipe(null, new Point(x + 1,y + 1))); }
+		if(way == null || way) { dfs(true, x + 1, y); }
+		if(way == null || !way){ dfs(false, x, y + 1); }
+		if(x + 1 < n && y + 1 < n && !metrix[x][y + 1] && !metrix[x + 1][y]) { dfs(null, x + 1,y + 1); }
 	}
 	
 	void run() {
-		if(!endChk()) { dfs(new Pipe(false,new Point(0,1))); }
+		if(!endChk()) { dfs(false,0,1); }
 		System.out.print(ans);
 	}
 }
 
-class Pipe {
-	Boolean way; // true : 세로 , false : 가로 , null : 대각
-	Point endPoint;
-	
-	Pipe(Boolean way, Point endPoint) {
-		this.way = way;
-		this.endPoint = endPoint;
-	}
-}
 
