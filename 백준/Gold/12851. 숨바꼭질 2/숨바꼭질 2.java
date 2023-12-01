03af42dd18109354ce19ba199d032ffb55380c48
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2023-12-01 10:41:51 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 196 ms, Memory: 59208 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -16,11 +16,11 @@ public class Main {
 
 class BOJ12851Sol {
 	
-	private int n, k;
-	private int[][] visit;
+	private int n, k, cnt;
+	private int[] visit;
 	
 	private void clear() {
-		visit = new int[100_001][2];
+		visit = new int[100_001];
 	}
 	
 	private void init() throws IOException {
@@ -33,38 +33,38 @@ class BOJ12851Sol {
 	private void bfs() {
 		Queue<Integer> queue = new LinkedList<>();
 		queue.add(n);
-		visit[n][0] = 1;
-		visit[n][1]++;
-		
+		visit[n] = 1;
+		countK(n);
 		while(!queue.isEmpty()) {
-			
 			int now = queue.remove();
+			int[] move = {now << 1, now + 1, now - 1};
 			
-			int[] move = {now * 2, now + 1, now - 1};
 			for(int i : move) {
-				
 				if(i < 0 || i > 100_000) {
 					continue;
 				}
-				
-				if(visit[i][0] > 0) {
-					// 방문한 경우
-					if(visit[i][0] == visit[now][0] + 1) {
-						visit[i][1]++;
+				if(visit[i] > 0) {
+					if(visit[i] == visit[now] + 1) {
 						queue.add(i);
+						countK(i);
 					}
 					continue;
 				}
-				
-				visit[i][0] = visit[now][0] + 1;
-				visit[i][1]++;
+				visit[i] = visit[now] + 1;
 				queue.add(i);
+				countK(i);
 			}
 		}
 	}
 	
+	private void countK(int i) {
+		if(i == k) {
+			cnt++;
+		}
+	}
+	
 	private void printAns() {
-		System.out.print(visit[k][0] - 1 + "\n" + visit[k][1]);
+		System.out.print(visit[k] - 1 + "\n" + cnt);
 	}
 	
 	void run() {
