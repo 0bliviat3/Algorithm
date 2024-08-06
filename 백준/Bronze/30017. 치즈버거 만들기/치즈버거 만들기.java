1a47ff36c79797700095ca62f59bc89735df882a
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2024-08-07 07:07:35 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 80 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            System.out.print(b * 2 + 1);
+            return;
+        }
+        System.out.print(a * 2 - 1);
+    }
+}
