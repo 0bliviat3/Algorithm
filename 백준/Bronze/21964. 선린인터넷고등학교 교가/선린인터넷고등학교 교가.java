8deaf8b05581d04469f7fe1827b443ac3c135640
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2024-09-21 19:22:09 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 72 ms, Memory: 13824 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(in.readLine());
+        String input = in.readLine().substring(size - 5);
+
+        System.out.print(input);
+    }    
+}
