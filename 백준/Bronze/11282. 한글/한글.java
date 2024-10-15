f7947004b76201e1050be508e047f8b055069344
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2024-10-16 07:06:05 +0900

    [Bronze IV] Title: 한글, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+
+        System.out.print((char) ('가' + n - 1));
+
+    }
+}
