615a18c72dcc370b3e43980c557b102dec8bd4c9
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2024-11-04 06:37:34 +0900

    [Bronze IV] Title: Simple Sum, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        st.nextToken();
+        int b = Integer.parseInt(st.nextToken());
+        st.nextToken();
+        int c = Integer.parseInt(st.nextToken());
+
+        if (a + b == c) {
+            System.out.print("YES");
+            return;
+        }
+        System.out.print("NO");
+    }
+}
