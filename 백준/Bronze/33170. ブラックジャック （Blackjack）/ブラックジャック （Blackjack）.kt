e5cd67adb1639f0bcfd0036a378f63c6a8493141
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-01-17 07:33:20 +0900

    [Bronze V] Title: ブラックジャック (Blackjack), Time: 80 ms, Memory: 12332 KB -BaekjoonHub

---
+++ 백준/Bronze/33170. ブラックジャック （Blackjack）/ブラックジャック （Blackjack）.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var a = readln().toInt()
+    a += readln().toInt()
+    a += readln().toInt()
+
+
+    print(
+        if (a <= 21) {
+            1
+        } else {
+            0
+        }
+    )
+}
