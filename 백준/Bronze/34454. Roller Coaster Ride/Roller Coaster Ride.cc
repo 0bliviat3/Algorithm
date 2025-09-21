04bc4bcef3fdd0b2f06788c4ca35e83c921f85ca
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-09-22 06:35:00 +0900

    [Bronze V] Title: Roller Coaster Ride, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34454. Roller Coaster Ride/Roller Coaster Ride.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int a, b, c;
+	std::cin >> a >> b >> c;
+	std::string ans = (a <= b * c) ? "yes" : "no";
+	std::cout << ans;
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
