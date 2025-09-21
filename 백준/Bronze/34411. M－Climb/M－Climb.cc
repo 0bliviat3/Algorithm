b1c277e4ef116a87528e461bb610816bb48892ec
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-09-21 11:27:34 +0900

    [Bronze V] Title: M-Climb, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int m, n, y;
+	std::cin >> m >> n >> y;
+	std::cout << m * n;
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
