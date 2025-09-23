9b7289326eb7e2c6efe9ca571c05179d54b0218e
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-09-24 06:48:02 +0900

    [Bronze V] Title: E-Days Ore Cart Pull, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34446. E－Days Ore Cart Pull/E－Days Ore Cart Pull.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int a;
+	std::cin >> a;
+	std::cout << (a << 1);
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
