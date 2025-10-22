da9b330c83e4c8f97c020fe073d0928d5bca5068
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-10-23 07:20:22 +0900

    [Bronze IV] Title: Quality-Adjusted Life-Year, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22279. Quality－Adjusted Life－Year/Quality－Adjusted Life－Year.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int n;
+	std::cin >> n;
+
+	double sum = 0.0;
+	while (n--) {
+		double a, b;
+		std::cin >> a >> b;
+		sum += a * b;
+	}
+	std::cout << sum;
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
