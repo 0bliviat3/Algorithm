875aabc8f00230f99a451bd841a41b6fa6bb324d
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-11-01 17:34:57 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int n;
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		std::cout << "Material Management " << i << "\n" << "Classification ---- End!\n";
+	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
