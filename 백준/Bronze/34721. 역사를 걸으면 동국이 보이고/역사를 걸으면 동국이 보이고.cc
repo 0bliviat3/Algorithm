a70e5cf3c94ce4bbf4159a9f9607b1b6b3233a62
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-11-10 14:50:27 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int n;
+	std::cin >> n;
+
+	while (n--) {
+		std::cout << "I love DGU\n";
+	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
