a3d97769a0a707907ea09ceb03b65dc680137b1d
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2025-12-28 13:53:38 +0900

    [Bronze III] Title: 크림빵, Time: 0 ms, Memory: 2020 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Bronze/28214. 크림빵/크림빵.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+void solve() {
+
+	int n, k, p;
+
+	std::cin >> n >> k >> p;
+
+	int m = n * k;
+	int count = 0;
+	int cnt = 0;
+	for (int i = 0; i < m; ++i) {
+		int x;
+		std::cin >> x;
+		if (i % k == 0) {
+			cnt = 0;
+		}
+		if (x) {
+			cnt++;
+		}
+		if ((i % k == k - 1) && cnt >= p) {
+			count++;
+		}
+	}
+
+	std::cout << count;
+
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
