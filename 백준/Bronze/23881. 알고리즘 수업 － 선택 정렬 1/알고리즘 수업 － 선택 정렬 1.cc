04047ca3c44cbc254b4050022fd1bda2153380c9
song-jae-wan <122261127+0bliviat3@users.noreply.github.com>
2026-02-10 07:09:31 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 56 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <climits>
+
+void solve() {
+
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<int> arr(n);
+
+	for (int i = 0; i < n; ++i) {
+		std::cin >> arr[i];
+	}
+
+	int cnt = 0;
+	for (int i = n - 1; i > 0; i--) {
+		int max = 0;
+		int max_idx = -1;
+		for (int j = 0; j <= i; j++) {
+			if (max < arr[j]) {
+				max = arr[j];
+				max_idx = j;
+			}
+		}
+		if (max != arr[i]) {
+			int tmp = arr[i];
+			arr[i] = max;
+			arr[max_idx] = tmp;
+			cnt++;
+			if (cnt == m) {
+				std::cout << arr[max_idx] << " " << arr[i] << "\n";
+				return;
+			}
+		}
+	}
+	std::cout << -1 << "\n";
+
+
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	solve();
+	return 0;
+}
